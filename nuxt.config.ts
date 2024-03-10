b43d7479c9b237576901523c4b5ccfd13703fb7b
Bence Molnar <xjarekx@gmail.com>
2024-03-10 12:33:51 +0100

    Add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,12 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   nitro: {
     preset: 'static'
-  }
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })

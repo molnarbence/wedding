6fd293337de2e9dae7970f56d3bf6e6a25d86b8c
Bence Molnar <xjarekx@gmail.com>
2024-03-10 13:03:14 +0100

    Update app.vue with wedding details

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <h1 class="text-3xl font-bold underline">
-    Hello world! Wow!
+    Réka és Bence esküvője
   </h1>
+  <div class="flex flex-col items-center justify-center">
+    <img
+      class="w-1/2"
+      src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0"
+      alt="esküvői kép"
+    />
+    <p class="text-xl mt-4">
+      Kedves Barátaink! Szeretettel várunk benneteket az esküvőnkön! 2024. szeptember 14-én 15:30-kor
+    </p>
+  </div>
 </template>

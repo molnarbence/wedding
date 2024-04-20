bb3a2372369b64e88f469c6ec4c967cbafa7b334
Bence Molnár <molnarbence@users.noreply.github.com>
2024-04-20 14:59:52 +0200

    Visszajelzes oldal skeleton

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,18 @@
     <div class="flex justify-center">
 
       <div class="max-w-5xl ">
+        <div class="flex items-center justify-center flex-col text-center">
+          <HeaderWithDeco></HeaderWithDeco>
+          <div class="text-center space-y-4 px-4 sm:px-0">
+                <p class="leading-1 text-gray-500 font-sans">
+                  <cite>Rejtelmek ha zengenek, <br>
+őrt állok, mint mesékbe'.<br>
+Bebujtattál engemet <br>
+talpig nehéz hűségbe.<br>
+</cite>
+                </p>
+            </div>
+        </div>
 
         <div class="flex items-center justify-center flex-col text-center">
           <HeaderWithDeco>Kedves családunk és barátaink</HeaderWithDeco>

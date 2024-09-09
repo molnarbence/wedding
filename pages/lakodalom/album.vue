ee88e8830becacb620b4113b98228eb0d214b94f
Bence Molnár <molnarbence@users.noreply.github.com>
2024-09-09 17:48:47 +0200

    fix format

--- pages/lakodalom/album.vue
+++ pages/lakodalom/album.vue
@@ -2,27 +2,21 @@
   <div>
     <ParagraphSection class="mb-5">
       <span>
-        Készítsetek nekünk és magatoknak fényképeket emlékül. <br> 
-        A bejáratnál találtok egy fényképalbumot, mellette egy fényképezőgéppel. <br>
-        A kép a fényképezőgép tetején fog kijönni, várjatok pár percet, amíg előhívódik a kép, <br>
-        majd tegyétek be az albumba a képet. <br>
-        Díszítsétek ki az oldalt, rajzoljatok mellé, írjatok mellé üzenetet, <br>
-        hogy még 30 év múlva is emlékezzünk erre a napra. <br>
+        Készítsetek nekünk és magatoknak fényképeket emlékül. <br />
+        A bejáratnál találtok egy fényképalbumot, mellette egy fényképezőgéppel. <br />
+        A kép a fényképezőgép tetején fog kijönni, várjatok pár percet, amíg előhívódik a kép, <br />
+        majd tegyétek be az albumba a képet. <br />
+        Díszítsétek ki az oldalt, rajzoljatok mellé, írjatok mellé üzenetet, <br />
+        hogy még 30 év múlva is emlékezzünk erre a napra. <br />
       </span>
     </ParagraphSection>
     <ParagraphSection class="mb-5">
       <HeaderWithDeco>Hogyan használjam?</HeaderWithDeco>
-      <span>
-        Az alábbi videó segít majd
-      </span>
+      <span> Az alábbi videó segít majd </span>
     </ParagraphSection>
   </div>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts" setup></script>
 
-</script>
-
-<style>
-
-</style>
+<style></style>

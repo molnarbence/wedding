e975db358762f5e5dfa88d54b3796885591e20f8
Bence Molnár <molnarbence@users.noreply.github.com>
2024-09-09 17:52:44 +0200

    Remove menu item

--- components/NaviMenu.vue
+++ components/NaviMenu.vue
@@ -2,8 +2,6 @@
   <ul>
     <NaviMenuItem to="/">Köszöntő</NaviMenuItem>
     <NaviMenuItem to="/lakodalom">Lakodalom</NaviMenuItem>
-    <NaviMenuItem to="/visszajelzes">Visszajelzés</NaviMenuItem>
-    <!-- <NaviMenuItem to="/roviden">Infók</NaviMenuItem> -->
   </ul>
 </template>
 

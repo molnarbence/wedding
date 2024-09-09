9def3a87f608c0f98859b08e4592c02454995bad
Bence Molnár <molnarbence@users.noreply.github.com>
2024-09-09 17:47:54 +0200

    album

--- components/LakodalomMenu.vue
+++ components/LakodalomMenu.vue
@@ -2,6 +2,7 @@
   <ul class="flex flex-row items-start justify-center gap-4 sm:gap-10 text-gray-700 text-lg sm:text-lg uppercase">
     <NaviMenuItem to="/lakodalom">Röviden</NaviMenuItem>
     <NaviMenuItem to="/lakodalom/menu">Menü</NaviMenuItem>
+    <NaviMenuItem to="/lakodalom/album">Album</NaviMenuItem>
   </ul>
 </template>
 
